612313c093777ecf4c3e8b515310e32e48ffb536
rgn-cream <reginanf7@gmail.com>
2024-11-08 13:32:26 +1200

    Menambahkan variabel tahun ini

--- Pert 2. Tugas 2.py
+++ Pert 2. Tugas 2.py
@@ -4,8 +4,8 @@ Kelas : 1B"""
 
 nama = input("Nama: ")
 tahunlahir = int(input("Tahun lahir: "))
-
-umur = 2024 - int(tahunlahir)
+tahun_ini = 2024 
+umur =tahun_ini - int(tahunlahir)
 
 print(f"Selamat datang, {nama}! Umur Anda sekarang adalah {umur}")
 
